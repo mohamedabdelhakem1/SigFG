99b3e91ae6317622ecc6004b41b725b27a88299f
muhammad salah <msalah.29.10@gmail.com>
2019-03-16 20:07:28 +0200

    initailized project

---
+++ src/drawer.js
@@ -0,0 +1,13 @@
+var addNodeOnClick = false;
+var addEdgeSelected = false;
+
+var nodes = new vis.DataSet([]);
+var edges = new vis.DataSet([]);
+
+function addNodeBtn() {
+	addNodeOnClick = true;
+}
+
+function addEdgeBtn() {
+	addEdgeSelected = true;
+}
---
+++ src/solver.js
@@ -0,0 +1,20 @@
+function solve(inNode, outNode){
+
+}
+
+function getForwardPaths(startNode, endNode){
+	//do dfs to end
+	//return list of list of edges
+}
+
+function getSuccessor(node){
+	//return list of nodes
+}
+
+function getCycles(Edges){
+	//return a list of list of edges depending on the given list of edges
+}
+
+function pathGain(edges){
+	//given a path return the gain (multiply weights)
+}
